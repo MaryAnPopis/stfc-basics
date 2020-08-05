db8a16980c268a710e2bffac1190cf8dae443d82
Mariam <maryanpopis@hotmail.com>
2020-08-04 18:13:24 -0600

    Add smooth active scroll

---
+++ index.js
@@ -0,0 +1,21 @@
+var selector = "nav a";
+$(window).scroll(function () {
+  onScrollt(selector);
+});
+function onScrollt(selector) {
+  var scrollPos = $(document).scrollTop();
+  $(selector).each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (
+      parseInt(refElement.position().top) <= scrollPos &&
+      parseInt(refElement.position().top) + parseInt(refElement.height()) >
+        scrollPos
+    ) {
+      $(selector).removeClass("active");
+      currLink.addClass("active");
+    } else {
+      currLink.removeClass("active");
+    }
+  });
+}

ce5b515f8288ed9144aa2de41f1bb2122a79c839
Rodrigo Carvajal Fernandez <rodrigof@amazon.com>
2020-09-06 13:27:57 -0700

    Added new content

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var selector = "nav a";
 $(window).scroll(function () {
-  onScrollt(selector);
+    onScrollt(selector);
 });
+
 function onScrollt(selector) {
-  var scrollPos = $(document).scrollTop();
-  $(selector).each(function () {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (
-      parseInt(refElement.position().top) <= scrollPos &&
-      parseInt(refElement.position().top) + parseInt(refElement.height()) >
-        scrollPos
-    ) {
-      $(selector).removeClass("active");
-      currLink.addClass("active");
-    } else {
-      currLink.removeClass("active");
-    }
-  });
+    var scrollPos = $(document).scrollTop();
+    $(selector).each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (
+            parseInt(refElement.position().top) <= scrollPos &&
+            parseInt(refElement.position().top) + parseInt(refElement.height()) >
+            scrollPos
+        ) {
+            $(selector).removeClass("active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
 }
